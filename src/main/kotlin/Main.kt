a0fe3c808f63337667f87a24bc33331b81d4484d
ana7293 <ana7293nik@gmail.com>
2023-03-10 23:55:04 +0500

    Pro Kotlin 3/5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    println("Здравствуй,")
+    /*println("Здравствуй,")
     println("Мир!")
 
     println("----------")
@@ -88,8 +88,143 @@ fun main() {
     val raaw3: String = readln()
     println(raaw3)
     println(raaw2)
-    println(raaw1)
+    println(raaw1)*/
 
+    val num1: Int = readln().toInt()                         // ввели первое число
+    val num2: Int = readln().toInt()                         // ввели второе число
+    println(num1 + num2)                                   // посчитали сумму и вывел
 
+    println("----------")
 
+    val a2: Int = 5
+    val b2: Int = 2
+    println(a2 + b2)                                      //бинарные арифметические операции
+    println(a2 - b2)
+    println(a2 * b2)
+
+    println("----------")
+
+    println(12 / 3)                                     //Оператор целочисленного деления
+    println(12 / 4)
+    println(12 / 5)
+    println(12 / 6)
+    println(12 / 7)
+    println(12 / 12)
+    println(12 / 14)
+
+    println("----------")
+
+    println(12 % 3)                                 //Оператор деления с остатком
+    println(12 % 4)
+    println(12 % 5)
+    println(12 % 6)
+    println(12 % 7)
+    println(12 % 12)
+    println(12 % 14)
+
+    println("----------")
+
+    val num2: Int = 2 + 2 * 2                        //порядок выполнения (приоритет) операций
+    println(num2)
+    val num3: Int = (2 + 2) * 2
+    println(num3)
+
+    println("----------")
+
+    var k = 0                                        //Унарные арифметические операторы
+    var l = 5
+
+    k++
+    l--
+
+    println("----------")
+
+    var num4: Int = readln().toInt()     //квадрат числа, считанного с консоли
+    println(num4 * num4)
+
+    println("----------")
+
+    var km: Int = readln().toInt()
+    println(km / 1000)
+
+    println("----------")
+
+    val st: Int = readln().toInt()          //число в 6 степени
+    val st1 = (st * st)
+    println(st1 * st1 * st1)
+
+    println("----------")
+
+    val d: Int = readln().toInt()
+    val t: Int = readln().toInt()
+    val s: Int = readln().toInt()
+    println(d * t * s)
+
+    println("----------")
+
+    val students: Int = readln().toInt()
+    val apples: Int = readln().toInt()
+    val fr = apples / students
+    println(fr)
+
+    println("----------")
+
+    val students1: Int = readln().toInt()
+    val apples1: Int = readln().toInt()
+    val fr1 = apples1 % students1
+    println(fr1)
+
+    println("----------")
+
+    val pt: Int = readln().toInt()
+    var pt1 = pt + 1
+    var pt2 = pt1 + 1
+    println(pt)
+    println(pt1)
+    println(pt2)
+
+    val ph1: Int = readln().toInt()
+    val ca1: Int = readln().toInt()
+    val he1: Int = readln().toInt()
+    val ch1: Int = readln().toInt()
+
+    val ph2: Int = readln().toInt()
+    val ca2: Int = readln().toInt()
+    val he2: Int = readln().toInt()
+    val ch2: Int = readln().toInt()
+
+    val ph3: Int = readln().toInt()
+    val ca3: Int = readln().toInt()
+    val he3: Int = readln().toInt()
+    val ch3: Int = readln().toInt()
+
+    val sum2: Int = ph2 + ca2 + he2 + ch2
+    val sum1: Int = ph1 + ca1 + he1 + ch1
+    val sum3: Int = ph3 + ca3 + he3 + ch3
+    println(sum1 + sum2 + sum3)
+
+    println("----------")
+
+    val rub: Int = readln().toInt()
+    val kop: Int = readln().toInt()
+    val cakes: Int = readln().toInt()
+    val sum = ((rub * 100) + kop) * cakes
+    val sumRub: Int = sum / 100
+    val sumKop: Int= sum % 100
+    println("$sumRub $sumKop")
+
+    println("----------")
+
+    val min: Int = readln().toInt()
+    val hour = min / 60
+    val ost = (min - hour * 60) % 60
+    println("$min мин - это $hour час $ost мин.")
+
+    println("----------")
+
+    val number: Int = readln().toInt()
+    val prev = number - 1
+    val next=  number + 1
+    println("Следующее за числом $number число: $next")
+    println("Для числа $number предыдущее число: $prev")
 }
