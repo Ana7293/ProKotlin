946d95703ef6f4ab745899182680cc2081a33f12
ana7293 <ana7293nik@gmail.com>
2023-04-09 02:10:15 +0500

    Pro Kotlin 5/8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     // Урок 5.7 Вложенные циклы
 
-     for (k in 1..3)  // выполняется 3 раза
+   /*  for (k in 1..3)  // выполняется 3 раза
      {
           for (i in 1..6) {
                print(5.toString() + " ")   // вывод на одной строке
@@ -79,6 +79,137 @@ fun main() {
         }
         i++
     }
+
+    println("----------") */
+
+   // Урок 5.8 Операторы break и continue
+
+    for (n in 1..10) {     //будет печатать только нечетные числа из-за срабатывания оператора continue
+        if (n % 2 == 0) {
+            continue
+        }
+        if (n == 7) {           //Цикл прекратит выполняться, когда n станет равно 7
+            break
+        }
+
+        println(n)
+    }
+
+    println("----------")
+
+    var ng = 1                //также будет печатать только нечетные числа из-за срабатывания continue
+    while (ng <= 10) {
+        if (ng % 2 == 0) {
+            ng++
+            continue
+        }
+        if (ng == 7) {        //Цикл прекратит выполняться, когда n станет равно 7
+            break
+        }
+
+        println(ng)
+        ng++
+    }
+
+    println("----------")
+
+    val slon = readln().toInt()            //Вывести максимальный делитель числа N, отличный от N (1вар)
+        var bym = 0
+        for (j in 1..slon) {
+            if (slon % j == 0 && j != slon) {
+               bym = j
+                continue
+            }
+        }
+        println(bym)
+
+    println("----------")
+
+    val nww = readln().toInt()                   //Вывести максимальный делитель числа N, отличный от N (2вар)
+
+    for (i in nww - 1 downTo 1) {
+        if (nww % i == 0) {
+            println(i)
+            break
+        }
+    }
+
+    println("----------")
+
+    val nqq = readln().toInt()                      //Вывести максимальный делитель числа N, отличный от N (3вар)
+    for (i in 2..nqq) {
+        if (nqq % i == 0) {
+            println(nqq / i)
+            break
+        }
+    }
+
+    println("----------")
+
+
+    for (i in 1..100)
+    {
+        val noch: Int = readln().toInt()
+        if (noch < 10) {
+            continue
+        }
+        if (noch > 100) {
+            break
+            }
+        println(noch)
+    }
+
+    println("----------")
+
+    var i = 0
+    var syt = 0
+    while (i + 1 < 10) {
+        i = i + 2
+        syt = syt + i
+        if (syt > 16) {
+            break
+        }
+    }
+    i = i + 1
+    println(i)
+
+    println("----------")
+
+    val nof: Int = readln().toInt()   //Вывести первое число от 1 до n кратное c, но НЕ кратное d.
+    val cof: Int = readln().toInt()
+    val dof: Int = readln().toInt()
+    for (u in 1..nof ) {
+        if ( u % cof == 0 && u % dof != 0) {
+            println(u)
+            break
+    }
+    }
+
+    println("----------")
+
+    val rer: String = readln().toString()   //По данной последовательности строк, определите какой по счету встретится строка
+    var xz = 1
+    while (true) {
+        val ter: String = readln().toString()
+        if (ter.compareTo(rer) == 0) {
+            break
+        }
+         xz++
+    }
+     println(xz)
+
+    println("----------")
+
+    val s = readln()    //По данной последовательности строк, определите какой по счету встретится строка (2вар)
+    var s1 = readln()
+    var count = 1
+
+    while (s != s1) {
+        count++
+        s1 = readln()
+    }
+
+    println(count)
 
     println("----------")
 
