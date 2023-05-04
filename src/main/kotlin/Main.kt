8cc06a9b9ef9a6bb4ceca6f50eba6ac36afd7adc
ana7293 <ana7293nik@gmail.com>
2023-05-05 00:39:40 +0500

    Pro Kotlin 7/2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,151 +1,161 @@
 fun main() {
-    // Урок 6.3 Итоговый тест
+    // Урок 7.2 Одномерные массивы. Работа с индексом
 
-    val s5: String = readln()               //Дана строка. Найдите k-ый символ в ней.
-    val n5r: Int = readln().toInt()
-    val countChar3: Int = s5.length
-    var byk = " "
-    if (n5r in 0..countChar3) {
-        for (index in 0 until countChar3) {
-            if (index == n5r - 1) {
-                byk = s5[index].toString()
-                println(byk)
-                break
+      val nf: Int = readln().toInt() // сколько чисел будут вводиться на следующей строке
+      val sf: String = readln() // сохранить все введенную строку
+      val ssf: Array<String> = sf.split(' ').toTypedArray() // разделить введенную строку по пробелам и сохранить в массив строкового типа
+
+      val numbersf = IntArray(nf)
+      for (i in 0 until nf) {
+          val number: Int = ssf[i].toInt()
+          numbersf[i] = number
+      }
+      var maxf = numbersf[0]
+      var indexMaxf = 0
+      for (i in 1 until nf) {
+          if (numbersf[i] > maxf) {
+              maxf = numbersf[i]
+              indexMaxf = i
+          }
+      }
+      println(maxf)             // Нахождение максимума, и индекса максимума
+      println(indexMaxf)
+
+      println("----------")
+
+      val s1: String = readln()        //Выводит перевернутый массив
+      val ss1: Array<String> = s1.split(' ').toTypedArray()
+      val length: Int = ss1.size
+      for (i in 0 until length / 2) {
+          val tmp: String = ss1[i]
+          ss1[i] = ss1[length - 1 - i]
+          ss1[length - 1 - i] = tmp
+      }
+      for (i in 0 until length) {
+          print(ss1[i] + " ")
+      }
+
+      println("----------")
+
+      val ng: Int = readln().toInt()    // сколько чисел будут вводиться на следующей строке
+      val sg: String = readln()        // сохранить все введенную строку
+      val ssg: Array<String> = sg.split(' ').toTypedArray() // разделить введенную строку по пробелам и сохранить в массив строкового типа
+      val numbers = IntArray(ng)
+      for (i in 0 until ng) {
+          val number: Int = ssg[i].toInt()
+          numbers[i] = number
+      }
+      var ming = numbers[0]   //минимальный элемент
+      var indexMing = 0
+      for (i in 1 until ng) {
+          if (numbers[i] < ming) {
+              ming = numbers[i]
+              indexMing = i
+          }
+      }
+      println(indexMing)    //Индекс минимального элемента
+
+      println("----------")
+
+    val nt: Int = readln().toInt()    //Уменьшить все элементы массива на минимальный элемент
+    val st: String = readln()
+    val sst: Array<String> = st.split(' ').toTypedArray()
+    val numberst = IntArray(nt)
+    for (i in 0 until nt) {
+        val number: Int = sst[i].toInt()
+        numbers[i] = number
+    }
+    var mint = numbers[0]   //минимальный элемент
+    for (i in 1 until nt) {
+        if (numbers[i] < mint) {
+            mint = numbers[i]
+        }
+    }
+    for (i in 0 until nt) {
+        val number: Int = sst[i].toInt()
+        numbers[i] = number - mint
+
+        print(numbers[i].toString() + " ")
+    }
+
+    println("----------")
+
+    val ny: Int = readln().toInt()    //Разность индексов максимума и минимума
+    val sy: String = readln()
+    val ssy: Array<String> = sy.split(' ').toTypedArray()
+    val numbersy = IntArray(ny)
+    for (i in 0 until ny) {
+        val number: Int = ssy[i].toInt()
+        numbers[i] = number
+    }
+    var miny = numbers[0]   //минимальный элемент
+    var indexMiny = 0       //индекс min
+    for (i in 1 until ny) {
+        if (numbers[i] < miny) {
+            miny = numbers[i]
+            indexMiny = i
+        }
+    }
+    var maxy = numbers[0]  //максимальный элемент
+    var indexMaxy = 0       //индекс max
+    for (i in 1 until ny) {
+        if (numbers[i] > maxy) {
+            maxy = numbers[i]
+            indexMaxy = i
+        }
+    }
+    println( indexMaxy - indexMiny)
+
+    println("----------")
+
+    val sa: String = readln()
+    val ssa: Array<String> = sa.split(' ').toTypedArray()
+    var cnta = 0
+    val na: Int = ssa.size
+    for (i in 0 until na) {
+        for (j in i + 1 until na) {
+            if (ssa[i] == ssa[j]) {
+                cnta++
             }
         }
-    } else println("NO")
+    }
+    println(cnta)
 
-    println("---------")
+    println("----------")
 
-    val z = readln().single()
-    val z2 = readln().single()
+    val nv: Int = readln().toInt()               // Поменять местами первый минимум и последний максимум
+    val s: String = readln()
+    val ss: Array<String> = s.split(' ').toTypedArray()
 
-    if (z2.code > z.code) {
-        for (g in z.code..z2.code) {                    //Часть алфавита
-            val h: Char = g.toChar()
-            print("$h ")
+
+    val numbersv = IntArray(nv)
+    for (i in 0 until nv) {
+        val number: Int = ss[i].toInt()
+        numbers[i] = number
+    }
+    var min = numbersv[0]
+    var max = numbersv[0]
+    var indexMinv = 0
+    var indexMaxv = 0
+    for (i in 1 until nv) {
+        if (numbers[i] < min) {
+            min = numbers[i]
+            indexMinv = i
         }
-    } else {
-        for (g in z2.code..z.code) {
-            val h: Char = g.toChar()
-            print("$h ")
+        if (numbers[i] >= max) {
+            max = numbers[i]
+            indexMaxv = i
         }
     }
+    val t = numbers[indexMaxv]
+    numbers[indexMaxv] = numbers[indexMinv]
+    numbers[indexMinv] = t
 
-    println("---------")
-
-    val s18: String = readln()           //Инициалы
-    val s28: String = readln()
-    val s38: String = readln()
-    val x8 = s28[0]
-    val x18 = s38[0]
-
-    print(s18 + " " + x8 + "." + x18 + ".")
-
-    println("---------")
-
-    val s9r: String = readln()
-    val s8r: String = readln()
-
-    val x77 = s9r[0]
-    val x177 = s8r[s8r.length-1]
-
-    if ( x177 == x77) {
-        println("YES")
-    } else {
-        println("NO")
+    for (i in 0 until nv) {
+        print("${numbers[i]} ")
     }
 
-    println("---------")
-
-    val smr66: String = readln()          //Гласные буквы
-    val x1 = smr66.length
-    var x66 = 0
-
-    for (i in 0 until x1) {
-        if (smr66[i].toChar() == 'a' || smr66[i].toChar() == 'e' || smr66[i].toChar() == 'i' || smr66[i].toChar() == 'o' || smr66[i].toChar() == 'u') {
-            x66++
-        }
-    }
-    print(x66)
-    println("---------")
-
-    val smr: String = readln()          //заменить буквы в строке
-    val result = smr.replace(
-        'e',
-        'i')
-
-     print(result)
-
-    println("---------")
-
-    val s1 = readln()                //Цифры в тексте
-
-    var s2 = ""
-
-    for (i in 0 until s1.length) {
-        if (s1[i].code > 47 && s1[i].code < 58 ) {
-            print(s1[i] + " ")
-        }
-    }
-
-    println("---------")
-
-    val n45 = readln().toInt()   //так, чтобы название не содержало xxx
-    val s45 = readln()
-
-    var countX = 0
-    var countXDelete = 0
-    for (i in 0 until s45.length) {
-        if (s45[i] == 'x') {
-            countX++
-        } else {
-            countX = 0
-        }
-
-        if (countX > 2) {
-            countXDelete++
-        }
-    }
-
-    println(countXDelete)
-
-    println("---------")
-
-    val n = readln().toInt()    //Слишком длинные слова
-    var x = ""
-    for ( j  in 0 until n) {
-        val s = readln()
-        val y = s.length
-    if (s.length > 10) {
-         x = s[0].toString() + (y.toInt() - 2) + s[y - 1].toString()
-        println(x)
-    } else {
-        println(s)
-    }
-        }
-
-    println("---------")
-
-    val s = readln()   //Азбука Арторзе
-    var i = 0
-    while (i < s.length) {
-        var number = 0
-        if (s[i] == '.') {
-            number = 0
-            i += 1
-        } else if (s[i + 1] == '.') {
-            number = 1
-            i += 2
-        } else {
-            number = 2
-            i += 2
-        }
-
-        print(number)
-    }
-
-    println("---------")
+     println("----------")
 
 
 
